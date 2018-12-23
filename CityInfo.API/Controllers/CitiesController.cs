11f15e44d82213b2b8754c8ebe22070e838d1c97
Rakesh Baruah <rbaruah@mac.com>
2018-12-22 22:05:16 -0600

    Added GetCity method on CityController

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -6,16 +6,22 @@ using System.Threading.Tasks;
 
 namespace CityInfo.API.Controllers
 {
+    [Route("api/cities")]
     public class CitiesController : Controller
     {
-        [HttpGet("api/cities")]
+        [HttpGet()]
         public JsonResult GetCities()
         {
-            return new JsonResult(new List<object>()
-            {
-                new {id = 1, Name = "New York City" },
-                new {id = 2, Name = "Chicago" }
-            });
+            return new JsonResult(CitiesDataStore.Current.Cities);
+
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(
+                CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id)
+                );
         }
     }
 }
