816a7e2a07d2ad7d23a54fcf4b3274ebe68bc28a
Rakesh Baruah <rbaruah@mac.com>
2018-12-23 00:03:56 -0600

    commented out json serializer in startup services to keep json properties camelCase in Postman.

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -15,14 +15,15 @@ namespace CityInfo.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc()
-                .AddJsonOptions(o =>
-                {
-                    if (o.SerializerSettings.ContractResolver != null)
-                    {
-                        var castedResolver = o.SerializerSettings.ContractResolver as DefaultContractResolver;
-                    }
-                });
+            services.AddMvc();
+                //.AddJsonOptions(o =>
+                //{
+                //    if (o.SerializerSettings.ContractResolver != null)
+                //    {
+                //        var castedResolver = o.SerializerSettings.ContractResolver as DefaultContractResolver;
+                //        castedResolver.NamingStrategy = null;
+                //    }
+                //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
