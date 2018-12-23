9c7433f7947073db4913e752b445e9fb8f8a533a
Rakesh Baruah <rbaruah@mac.com>
2018-12-22 21:49:52 -0600

    Added HTTP GET method w/ API call test via Postman.

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -12,9 +12,9 @@ namespace CityInfo.API
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +24,8 @@ namespace CityInfo.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMvc();
 
             app.Run(async (context) =>
             {
